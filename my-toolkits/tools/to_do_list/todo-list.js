b8a1fa860a1c1e60e67e1bcbd4fe2c21ab316e19
root <root@localhost.localdomain>
2022-01-18 03:22:22 -0500

    to do list fully done

--- my-toolkits/tools/to_do_list/todo-list.js
+++ my-toolkits/tools/to_do_list/todo-list.js
@@ -4,36 +4,83 @@ $(document).ready(function () {
         $('.modal').modal();
     });
 });
+
+
+const container = document.querySelector('.containerj');
+var inputValue = document.querySelector('.inputj');
+const add = document.querySelector('#addclick');
+
+if (window.localStorage.getItem("todos") == undefined) {
+    var todos = [];
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+var todosEX = window.localStorage.getItem("todos");
+var todos = JSON.parse(todosEX);
+
+
+class item {
+    constructor(name) {
+        this.createItem(name);
+    }
+    createItem(name) {
+        
+        var listitems = document.createElement('li');
+        listitems.className = 'collection-item';
+
+        var icon1 = document.createElement('i');
+        icon1.className = 'material-icons deep-purple-text text-darken-3 left';
+        icon1.innerHTML = "description";
+
+
+        var taskname = document.createElement('div');
+        taskname.innerHTML = name;
+
+        var remove = document.createElement('i');
+        remove.className = 'material-icons red-text cdel right';
+        remove.innerHTML = 'delete_forever';
+        remove.addEventListener('click', () => this.remove(listitems, name));
+
+        container.appendChild(listitems);
+        listitems.appendChild(icon1);
+        listitems.appendChild(taskname);
+        
+        taskname.appendChild(remove);
+
+        
+
+    }
+
     
-// var init 
-var selectedTask = [];
+    remove(itemBox, name) {
+        itemBox.parentNode.removeChild(itemBox);
+        let index = todos.indexOf(name);
+        todos.splice(index, 1);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+    }
+}
 
-    // open a new form take a name and add it to the new list
-    $("#addclick").click(function () {
-        var task_title = document.getElementById("task_name").value.toString();
-        // console.log(task_title);
-        var t = '<li class="collection-item"><i class="material-icons deep-purple-text text-darken-3 left">description</i><div>'+ task_title +'<a href="#!" class="secondary-content"><i class="material-icons red-text cdel" id="cdelete">delete_forever</i></a></div></li>';
+add.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+    if (e.which == 13) {
+        check();
+    }
+})
 
-        $(".collection").append(t);
-        app.selectedTask.push({
-            task_title: task_name
-        });
-        var selectedTasks = JSON.stringify(app.selectedTasks);
-        localStorage.selectedTasks = selectedTasks;
-        // console.log("local storage done");
-    });
+function check() {
+    if (inputValue.value != "") {
+        new item(inputValue.value);
+        todos.push(inputValue.value);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+        inputValue.value = "";
+    }
+}
 
-    $(document).on("click", "i.cdel", function () {
-        var p = $(this).parent().parent()['0']['firstChild']['data'];
-        var sel = [];
-        app.selectedTasks.forEach(function(element){
-          console.log(element['task_title']);
-          if (element['task_title']!=p)
-            sel.push(element);
-        });
-        app.selectedTasks = sel;
-        var selectedTasks = JSON.stringify(app.selectedTasks);
-        localStorage.selectedTasks = selectedTasks;    
-        $(this).parent().parent().parent().remove();
-      });
-    
+
+for (var v = 0; v < todos.length; v++) {
+    new item(todos[v]);
+}
+
+
+
+
